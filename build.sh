2736c0c2f8ff9d6dd6cf1c433f1f174ab4012eda
Lif <ThreadSpeed@gmail.com>
2021-09-27 19:11:18 -0700

    Created templates, and created bash build script.

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat ./templates/top.html ./content/index.html ./templates/bottom/html > ./docs/index.html
+cat ./templates/top.html ./content/testimonials.html ./templates/bottom/html > ./docs/testimonials.html
+cat ./templates/top.html ./content/who.html ./templates/bottom/html > ./docs/who.html
+cat ./templates/top.html ./content/wip.html ./templates/bottom/html > ./docs/wip.html
